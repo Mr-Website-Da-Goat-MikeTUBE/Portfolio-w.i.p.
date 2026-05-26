b5e290e594fe8d261c9b2a7caaa5b62c39065bea
Mr-Website-Da-Goat-MikeTUBE <mapworking01@gmail.com>
2026-05-26 07:24:46 -0400

    Add files via upload

--- menuu.js
+++ menuu.js
@@ -1,19 +1,26 @@
 function myFunction() {
-      var x = document.getElementById("testid");
+      var x = document.getElementById("testdiv");
 
-      if (x.style.display === "none") {
+    x.addEventListener("animationend", function(e) {
+        e.preventDefault();
+
+        if(x.style.animationName == "slide-left") {
+            x.style.display = "none";
+        }
+    });
+
+    if (x.style.display === "none") {
         x.style.display = "block";
         x.style.animation = "1s slide-right";
         x.style.transform = "translateX(40%)";
-      } else {
-        ;
-     }
-    }
+        }
+
+}
 
 function myFunction2() {
-  var x = document.getElementById("testid");
-
-        x.style.animation = "1s slide-left";
-        if (x.style.transform = "translateX(100%)"){x.style.display = "none"}
-        
-   }
+      var x = document.getElementById("testdiv");
+            
+      {
+          x.style.animation = "1s slide-left";
+      }
+}
